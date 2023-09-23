0b6592e07f8d7d325f629b41fd98350ed458f437
Tran Cong Viet An <trancongvietan22@gmail.com>
2023-09-24 02:28:55 +0700

    feat: add notification

--- src/backup.sh
+++ src/backup.sh
@@ -1,51 +1,74 @@
 #! /bin/sh
 
-set -eu
+set -u
 set -o pipefail
 
 source ./env.sh
 
-echo "Creating backup of $POSTGRES_DATABASE database..."
-pg_dump --format=custom \
-        -h $POSTGRES_HOST \
-        -p $POSTGRES_PORT \
-        -U $POSTGRES_USER \
-        -d $POSTGRES_DATABASE \
-        $PGDUMP_EXTRA_OPTS \
-        > db.dump
+OUTPUT=""
 
-timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
-s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
+function log() {
+  curr_output=$(echo $1 | tee /dev/tty)
+  OUTPUT="$OUTPUT\n$curr_output"
+}
 
-if [ -n "$PASSPHRASE" ]; then
-  echo "Encrypting backup..."
-  rm -f db.dump.gpg
-  gpg --symmetric --batch --passphrase "$PASSPHRASE" db.dump
-  rm db.dump
-  local_file="db.dump.gpg"
-  s3_uri="${s3_uri_base}.gpg"
+function try_backup() {
+  log "Creating backup of $POSTGRES_DATABASE database..."
+  pg_dump --format=custom \
+          -h $POSTGRES_HOST \
+          -p $POSTGRES_PORT \
+          -U $POSTGRES_USER \
+          -d $POSTGRES_DATABASE \
+          $PGDUMP_EXTRA_OPTS \
+          > db.dump
+
+  timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
+  s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
+
+  if [ -n "$PASSPHRASE" ]; then
+    log "Encrypting backup..."
+    rm -f db.dump.gpg
+    gpg --symmetric --batch --passphrase "$PASSPHRASE" db.dump
+    rm db.dump
+    local_file="db.dump.gpg"
+    s3_uri="${s3_uri_base}.gpg"
+  else
+    local_file="db.dump"
+    s3_uri="$s3_uri_base"
+  fi
+
+  log "Uploading backup to $S3_BUCKET..."
+  aws $aws_args s3 cp "$local_file" "$s3_uri"
+  log "Uploaded dump file to $s3_uri"
+  rm "$local_file"
+
+  log "Backup complete."
+
+  if [ -n "$BACKUP_KEEP_DAYS" ]; then
+    sec=$((86400*BACKUP_KEEP_DAYS))
+    date_from_remove=$(date -d "@$(($(date +%s) - sec))" +%Y-%m-%d)
+    backups_query="Contents[?LastModified<='${date_from_remove} 00:00:00'].{Key: Key}"
+
+    log "Removing old backups from $S3_BUCKET..."
+    aws $aws_args s3api list-objects \
+      --bucket "${S3_BUCKET}" \
+      --prefix "${S3_PREFIX}" \
+      --query "${backups_query}" \
+      --output text \
+      | xargs -n1 -t -I 'KEY' aws $aws_args s3 rm s3://"${S3_BUCKET}"/'KEY'
+    log "Removal complete."
+  fi
+}
+
+try_backup 
+
+if [ $? -ne 0 ]; then
+  RESULT="failed"
 else
-  local_file="db.dump"
-  s3_uri="$s3_uri_base"
+  RESULT="success"
 fi
 
-echo "Uploading backup to $S3_BUCKET..."
-aws $aws_args s3 cp "$local_file" "$s3_uri"
-rm "$local_file"
-
-echo "Backup complete."
-
-if [ -n "$BACKUP_KEEP_DAYS" ]; then
-  sec=$((86400*BACKUP_KEEP_DAYS))
-  date_from_remove=$(date -d "@$(($(date +%s) - sec))" +%Y-%m-%d)
-  backups_query="Contents[?LastModified<='${date_from_remove} 00:00:00'].{Key: Key}"
-
-  echo "Removing old backups from $S3_BUCKET..."
-  aws $aws_args s3api list-objects \
-    --bucket "${S3_BUCKET}" \
-    --prefix "${S3_PREFIX}" \
-    --query "${backups_query}" \
-    --output text \
-    | xargs -n1 -t -I 'KEY' aws $aws_args s3 rm s3://"${S3_BUCKET}"/'KEY'
-  echo "Removal complete."
+if [ -n "$NOTIFICATION_URL" ]; then
+  python3 -c "import requests; requests.post('$NOTIFICATION_URL', json={'message': '$OUTPUT', 'result': '$RESULT'})"
 fi
+
--- src/install.sh
+++ src/install.sh
@@ -13,7 +13,7 @@ apk add gnupg
 
 apk add python3
 apk add py3-pip  # separate package on edge only
-pip3 install awscli
+pip3 install awscli requests
 
 # install go-cron
 apk add curl
